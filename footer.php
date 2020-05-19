6bcb72d2b16146a4c1c1131024619a96b9dcd2a3
sgfanega <steve.fanega.ii@gmail.com>
2020-05-18 21:37:48 -0500

    Updates on scss and footer.php

--- footer.php
+++ footer.php
@@ -5,17 +5,17 @@
         </div>
         <div class="container d-flex justify-content-end">
             <div class="row">
-                <div class="col-sm">
+                <div class="col">
                     <a href="https://www.linkedin.com/in/steve-fanega-ii/">
                         <img class="social_media_logo" src="http://stevefanegaii.test/wp-content/uploads/2020/05/LinkedIn-Logo.png">
                     </a>
                 </div>
-                <div class="col-sm">
+                <div class="col">
                     <a href="https://www.instagram.com/notstevefanega/">
                         <img class="social_media_logo" src="http://stevefanegaii.test/wp-content/uploads/2020/05/Instagram-Logo.png">
                     </a>
                 </div>
-                <div class="col-sm">
+                <div class="col">
                     <a href="https://github.com/sgfanega">
                         <img class="social_media_logo" src="http://stevefanegaii.test/wp-content/uploads/2020/05/GithubLogo.png">
                     </a>
