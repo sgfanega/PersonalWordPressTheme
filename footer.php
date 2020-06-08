0611d088043018a48ce01bc35f3d27b308d86631
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-07 22:20:35 -0500

    Added Breaks

--- footer.php
+++ footer.php
@@ -1,23 +1,23 @@
     <?php wp_footer();?>
     <div class="container d-flex justify-content-center footer">
         <div class="container d-flex jusitfy-content-start">
-            <h5>&copy;Steve Fanega</h5>
+            <h5>&copy; 2020 Steve Fanega All Rights Reserved</h5>
         </div>
         <div class="container d-flex justify-content-end">
             <div class="row">
                 <div class="col">
                     <a href="https://www.linkedin.com/in/steve-fanega-ii/">
-                        <img class="social_media_logo" src="http://stevefanegaii.test/wp-content/uploads/2020/05/LinkedIn-Logo.png">
+                        <img class="social_media_logo" src="http://stevefanegaii.com/wp-content/uploads/2020/05/LinkedIn-Logo.png">
                     </a>
                 </div>
                 <div class="col">
                     <a href="https://www.instagram.com/notstevefanega/">
-                        <img class="social_media_logo" src="http://stevefanegaii.test/wp-content/uploads/2020/05/Instagram-Logo.png">
+                        <img class="social_media_logo" src="http://stevefanegaii.com/wp-content/uploads/2020/05/Instagram-Logo.png">
                     </a>
                 </div>
                 <div class="col">
                     <a href="https://github.com/sgfanega">
-                        <img class="social_media_logo" src="http://stevefanegaii.test/wp-content/uploads/2020/05/GithubLogo.png">
+                        <img class="social_media_logo" src="http://stevefanegaii.com/wp-content/uploads/2020/05/GithubLogo.png">
                     </a>
                 </div>
             </div>
